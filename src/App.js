e3029fb2f2fbd0664ae7e7224ffae5b236970c43
Eszti2222 <nagy.eszter2222@gmail.com>
2026-02-06 12:18:26 +0100

    frontend alapok

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import CustomerLayout from "../../frontend/src/layouts/CustomerLayot";
+import HomePage from "../../frontend/src/pages/HomePage";
+import RegisterPage from "../../frontend/src/pages/RegisterPage";
+import LoginPage from "../../frontend/src/pages/LoginPage";
+import PatientHomePage from "../../frontend/src/pages/PatientHomePage";
+
+function App() {
+    return (<div>fjdalkjf dskfj</div>
+        
+    );
+}
+
+export default App;
+
+
+{/* <Routes>
+            <Route path="/" element={<CustomerLayout />}>
+                <Route index element={<HomePage />} />
+                
+                <Route path="bejelentkezes" element={<LoginPage />} />
+                <Route path="regisztracio" element={<RegisterPage />} />
+                <Route path="patient" element={<PatientHomePage />} />
+
+            </Route>
+        </Routes> */}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./services/App";
-import { AuthProvider } from "./contexts/AuthContext"; // importáljuk
+import { AuthProvider } from "./contexts/AuthContext"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,3 +15,23 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
+*/
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { AuthProvider } from "../../frontend/src/contexts/AuthContext";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+/*  <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter> */
